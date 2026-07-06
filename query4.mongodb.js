51d4330c7c22ac6f579aab81f487578fbdd3874d
saokeems <saokeems@gmail.com>
2026-07-06 15:57:35 +0700

    updated Ex 3 in file query4

--- query4.mongodb.js
+++ query4.mongodb.js
@@ -3,4 +3,10 @@ use("sample_mflix");
 // Ex 3 theaters
 
 // count theaters AL state
-db.theaters.find({ "location.address.state": "AL" }).count();
+// db.theaters.find({ "location.address.state": "AL" }).count();
+
+// count theaters La Quinta city
+// db.theaters.find({ "location.address.city": "La Quinta" }).count();
+
+// Ex one from La Quinta city
+db.theaters.findOne({ "location.address.city": "La Quinta" });
