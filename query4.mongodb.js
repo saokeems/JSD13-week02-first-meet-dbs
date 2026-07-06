e21a7d4b19883ec104dffbb2422cca7af2b2649d
saokeems <saokeems@gmail.com>
2026-07-06 20:57:27 +0700

    added query4

---
+++ query4.mongodb.js
@@ -0,0 +1,12 @@
+use("sample_mflix");
+
+// Ex4
+
+// count movies mentioned american plot
+// db.movies.find({ plot: { $regex: "American", $options: "i" } }).count();
+
+// count movies end plot with word street
+// db.movies.find({ plot: { $regex: "street.$", $options: "i" } }).count();
+
+// Ex data from above
+db.movies.find({ plot: { $regex: "street.$", $options: "i" } });
